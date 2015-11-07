52ee7a87911903041791bf8ffc55c4b3e26bda0c
Brian Lai <b2lai@uwaterloo.ca>
2015-11-07 13:54:40 -0500

    woo

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Learning C harder than [Learn C the Hard Way][2].
 
 ## Installing
 
-`make`, probably. Assuming it doesn't throw any warnings. Which it does.
+`sudo apt-get install build-essential splint valgrind`
+
+## Running
+
+`make run`, probably. Assuming it doesn't throw any warnings.
 
 [1]: http://www.splint.org/sponsors.html
 [2]: http://c.learncodethehardway.org/
--- ex.c
+++ ex.c
@@ -1,13 +1,50 @@
 #include <stdio.h>
 
+/**
+ * Tabs are 8 characters, and thus indentations are also 8 characters.
+ * There are heretic movements that try to make indentations 4 (or even 2!)
+ * characters deep, and that is akin to trying to define the value of PI to
+ * be 3.
+ *
+ * Now, some people will claim that having 8-character indentations makes
+ * the code move too far to the right, and makes it hard to read on a
+ * 80-character terminal screen.  The answer to that is that if you need
+ * more than 3 levels of indentation, you're screwed anyway, and should fix
+ * your program.
+ *
+ * https://www.kernel.org/doc/Documentation/CodingStyle
+ */
 int main(void) {
-    double age = 10.0;
+	double age = 10.0;
 
-    /*In the strict library, [printing functions] are declared to return int, 
-     *so ignored return value errors will be reported
-     */
-    (void) puts("Hello world.");
-    (void) printf("I am %f years old.\n", age);
+	/**
+	 * C is a Spartan language, and so should your naming be.  Unlike Modula-2
+	 * and Pascal programmers, C programmers do not use cute names like
+	 * ThisVariableIsATemporaryCounter.  A C programmer would call that
+	 * variable "tmp", which is much easier to write, and not the least more
+	 * difficult to understand.
+	 */
+	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
 
-    return 0;
+	int idx = 0;
+	int areas[] = {1, 2, 3, 4, 5};
+
+	/**
+	 * In the strict library, [printing functions] are declared to
+	 * return int, so ignored return value errors will be reported
+	 */
+	(void) puts("Hello world.");
+	(void) printf("I am %f years old.\n", age);
+
+	(void) printf(
+		"The entire universe has %ld (%e) bugs.\n",
+		universe_of_defects,
+		(double) universe_of_defects
+	);
+
+	for (idx = 0; idx < 5; idx++) {
+		(void) printf("%d", areas[idx]);
+	}
+
+	return 0;
 }
